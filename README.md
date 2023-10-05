b8a9efc01fbdaec996ce8a907131a700979bf099
JSman225 <leonardikarl@gmail.com>
2023-10-05 19:25:08 -0400

    Started work.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ROTL
 Railroads Online Track Laying
+
+<!-- npx tailwindcss -i ./tailwind/input.css -o ./tailwind/output.css --watch -->
---
+++ general.js
@@ -0,0 +1,7 @@
+function toggleMenuVisibility() {
+    document.getElementById("mobileMenu").classList.toggle("hidden");
+}
+function toggleMenu() {
+    document.getElementById("menuIcon").classList.toggle("rotate-180");
+    document.getElementById("menu").classList.toggle("hidden");
+}
