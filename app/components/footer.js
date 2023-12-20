b725b50a55cadb2edf4c0ea740dca85a07d67cb4
JSman225 <leonardikarl@gmail.com>
2023-12-19 22:11:17 -0500

    footer + policies

--- app/components/footer.js
+++ app/components/footer.js
@@ -30,10 +30,10 @@ export default function Footer() {
               <h2 class="mb-6 text-sm font-semibold uppercase text-white">Resources</h2>
               <ul class="text-gray-400 font-medium">
                 <li class="mb-4">
-                  <a href="#" class="hover:underline">Flowbite</a>
+                  <a href="https://www.railroadsonline.com/en/" class="hover:underline">Railroads Online</a>
                 </li>
                 <li>
-                  <a href="#" class="hover:underline">Tailwind CSS</a>
+                  <a href="https://store.steampowered.com/app/1696810/Railroads_Online/" class="hover:underline">Steam Store</a>
                 </li>
               </ul>
             </div>
@@ -41,10 +41,10 @@ export default function Footer() {
               <h2 class="mb-6 text-sm font-semibold uppercase text-white">Follow us</h2>
               <ul class="text-gray-400 font-medium">
                 <li class="mb-4">
-                  <a href="#" class="hover:underline ">Github</a>
+                  <a href="https://discord.gg/EjzW9uQVsk" class="hover:underline ">Discord Community</a>
                 </li>
                 <li>
-                  <a href="#" class="hover:underline">Discord</a>
+                  <a href="https://twitter.com/railroadsgame" class="hover:underline">Twitter</a>
                 </li>
               </ul>
             </div>
@@ -52,7 +52,7 @@ export default function Footer() {
               <h2 class="mb-6 text-sm font-semibold uppercase text-white">Legal</h2>
               <ul class="text-gray-400 font-medium">
                 <li class="mb-4">
-                  <a href="#" class="hover:underline">Privacy Policy</a>
+                  <a href="/privacy-policy" class="hover:underline">Privacy Policy</a>
                 </li>
                 <li>
                   <a href="#" class="hover:underline">Terms &amp; Conditions</a>
@@ -64,19 +64,14 @@ export default function Footer() {
         <hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
           <span class="text-sm sm:text-center text-gray-300">© 2023 <a href="https://flowbite.com/"
-              class="hover:underline">RO Track Laying™</a>. All Rights Reserved.
+            class="hover:underline">RO Track Laying™</a>. All Rights Reserved.
           </span>
           <div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-            <a href="#" class="text-gray-500 hover:text-white">
-              <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 8 19">
-                <path fill-rule="evenodd"
-                  d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <span class="sr-only">Facebook page</span>
+            <a href="mailto:masonkpiano@outlook.com" class="text-gray-500 hover:text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="currentColor" dataSlot="icon" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" /></svg>
+              <span class="sr-only">Email</span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-white">
+            <a href="https://discord.gg/EjzW9uQVsk" class="text-gray-500 hover:text-white">
               <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                 viewBox="0 0 21 16">
                 <path
@@ -84,35 +79,30 @@ export default function Footer() {
               </svg>
               <span class="sr-only">Discord community</span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-white">
+            <a href="https://discord.gg/Y52PW9cXpR" class="text-gray-500 hover:text-white">
               <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 17">
-                <path fill-rule="evenodd"
-                  d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                  clip-rule="evenodd" />
+                viewBox="0 0 21 16">
+                <path
+                  d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
               </svg>
-              <span class="sr-only">Twitter page</span>
+              <span class="sr-only">Discord community</span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-white">
-              <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path fill-rule="evenodd"
-                  d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <span class="sr-only">GitHub account</span>
+            <a href="https://twitter.com/railroadsgame" class="text-gray-500 hover:text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="currentColor" dataSlot="icon" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+              <span class="sr-only">Twitter link</span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-white">
-              <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path fill-rule="evenodd"
-                  d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
-                  clip-rule="evenodd" />
-              </svg>
-              <span class="sr-only">Dribbble account</span>
+            
+            <a href="https://www.railroadsonline.com/en/" class="text-gray-500 hover:text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="currentColor" dataSlot="icon" viewBox="0 0 640 512"><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg>
+              <span class="sr-only">Outside link</span>
+            </a>
+            <a href="https://store.steampowered.com/app/1696810/Railroads_Online/" class="text-gray-500 hover:text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="currentColor" dataSlot="icon" viewBox="0 0 640 512"><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg>
+              <span class="sr-only">Outside link</span>
             </a>
           </div>
         </div>
       </div>
     </footer>
-)}
+  )
+}
---
+++ app/privacy-policy/page.js
@@ -0,0 +1,89 @@
+export default function serverManager() {
+    return (
+        <main className="relative bg-white">
+            <section className="text-black p-28 mx-auto max-w-7xl">
+                <h1 className="text-4xl font-medium">Privacy Policy for RO Track Laying</h1>
+                <p>Last updated: <b>12/19/2023</b></p>
+
+                <br />
+                <div>
+                    <h2 className="text-3xl font-medium">Definitions</h2>
+
+                    <br />
+                    <div>
+                        <h3 className="text-xl font-medium">"You"</h3>
+                        <ul className="text-lg list-disc pl-12">
+                            <li>
+                                "You" refers to the individual accessing or using the Website, or Service, or the company or any other legal entity on behalf of which such individual is accessing or using the Website, or Service, as applicable.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <br />
+                <div>
+                    <h2 className="text-3xl font-medium">I: General Privacy Policy</h2>
+
+                    <br />
+                    <div>
+                        <h3 className="text-xl font-medium">Website Cookies</h3>
+                        <ul className="text-lg list-disc pl-12">
+                            <li>
+                                Cookies are used to help analytics and color modes run on this site.
+                            </li>
+                            <li>
+                                We comply with state laws such as CCPA (California), VCDPA & CDPA (Virginia), CPA (Colorado), CTDPA (Connecticut), and UCPA (Utah), as well as GDPR in Europe. Users have the option to block cookies at any time.
+                            </li>
+                        </ul>
+                    </div>
+
+                    <br />
+                    <div>
+                        <h3 className="text-xl font-medium">Data and Files</h3>
+                        <ul className="text-lg list-disc pl-12">
+                            <li>
+                                We do not log user activity, and any data that reaches us, including data from cookies, will not be sold.
+                            </li>
+                            <li>
+                                Users can request the removal of their claimed data/files by contacting our support line.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <br />
+                <div>
+                    <h2 className="text-3xl font-medium">II: Acceptance</h2>
+                    <p className="text-lg">By visiting this site, You signify your agreement to the following Privacy Policy.</p>
+                </div>
+
+                <br />
+                <div>
+                    <h2 className="text-3xl font-medium">Contact Information</h2>
+                    <p className="text-lg">
+                        Users with questions or concerns can join our Discord server: <a className="text-sky-800" href="https://discord.gg/EjzW9uQVsk">RO Track Laying Discord</a>.
+                        <br />
+                        <ul className="text-lg list-disc pl-12">
+                            <li>
+                                Direct any inquiries to <a className="text-sky-800" href="mailto:masonkpiano@outlook.com">masonkpiano@outlook.com</a>.
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+
+                <br />
+                <div>
+                    <h2 className="text-3xl font-medium">Changes to this Privacy Policy</h2>
+                    <p className="text-lg">We may update our privacy policy from time to time. Any changes will be posted on this page.</p>
+                    <p className="text-lg">Your use of this website implies acceptance of this Privacy Policy.</p>
+                </div>
+
+                <br />
+                <p className="text-lg">If you have any questions or concerns, please contact us via the provided channels.</p>
+                <br />
+                <p className="text-lg">Thank you for visiting RO Track Laying!</p>
+            </section>
+        </main>
+    )
+}
+
