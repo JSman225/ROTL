9ba164de2d2b9c88e576f74d9785e33844be89b6
JSman225 <leonardikarl@gmail.com>
2024-06-16 18:38:50 -0400

    Update page.js

--- app/server-manager/page.js
+++ app/server-manager/page.js
@@ -10,9 +10,9 @@ export default function serverManager() {
                 <a href="https://discord.gg/KaVKdDnA6D"
                     className="sm:ml-28 shadow-large mt-12 flex items-center gap-7 justify-center bg-[#47E5C1] w-11/12 h-20 sm:w-[420px] sm:h-[86px] text-[#111111] rounded-2xl font-semibold text-xl sm:text-2xl">
                     Download for free
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"
                         className="w-8 h-8">
-                        <path stroke-linecap="round" stroke-linejoin="round"
+                        <path strokeLinecap="round" strokeLinejoin="round"
                             d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                     </svg>
                 </a>
@@ -53,9 +53,9 @@ export default function serverManager() {
                                 <a href="https://www.fantasytechs.com/rotlm"
                                     className="mt-8 mx-auto flex items-center gap-7 justify-center bg-[#47E5C1] w-11/12 h-16 py-8 sm:w-80 text-[#111111] rounded-xl font-semibold text-xl">
                                     Download for free
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5"
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5"
                                         stroke="currentColor" className="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                        <path strokeLinecap="round" strokeLinejoin="round"
                                             d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                                     </svg>
                                 </a>
